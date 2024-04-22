94aaf711ae44bfbb12b229d8657b50dc2f94e057
stirling campbell <3212918+stirlo@users.noreply.github.com>
2024-04-22 18:56:42 +1000

    change the script a little ; removed one line.. see If it works though

--- script.js
+++ script.js
@@ -13,13 +13,12 @@ document.addEventListener("DOMContentLoaded", function() {
             .then(response => response.json())
             .then(data => {
                 const images = data.filter(file => /\.(jpe?g)$/i.test(file.name)).map(file => file.download_url);
-                sections.forEach((section, index) => {
-                    const imageContainer = section.querySelector('.background-image-container');
+                sections.forEach(section => {
                     const randomImage = images[Math.floor(Math.random() * images.length)];
-                    imageContainer.style.backgroundImage = `url(${randomImage})`;
-                    imageContainer.classList.add('image-adjust');
-                    imageContainer.style.backgroundRepeat = 'no-repeat';
-                    imageContainer.style.backgroundSize = 'cover';
+                    section.style.backgroundImage = `url(${randomImage})`;
+                    section.classList.add('image-adjust');
+                    section.style.backgroundRepeat = 'no-repeat';
+                    section.style.backgroundSize = 'cover';
                 });
             })
             .catch(error => console.error('Error loading images:', error));
